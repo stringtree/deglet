f4499299d1ceb9e49ab38ef7fca13051eb208dd6
Frank Carver <frank@efsol.com>
2016-08-30 17:19:57 +0100

    move a todo to tests

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ module.exports.createFromObject = function createFromObject(object) {
   return new Deglet(object.year, object.month, object.day); 
 };
 
-//TODO createFromISOString
-
 const days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const leap_years = [
   1904, 1908, 1912, 1916, 1920, 1924, 1928, 1932, 1936, 1940, 1944, 1948,
